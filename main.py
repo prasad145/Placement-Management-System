47d750ee3fe0cf67543023fb647f353b7adf8606
Prasad acharya <50115378+prasad145@users.noreply.github.com>
2020-10-14 10:17:26 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask , render_template , redirect , url_for , request
+import xlrd
+
+app = Flask(__name__)
+
+@app.route('/home')
+
+def home():
+    return (render_template('home.html'))
+@app.route('/admin_home')
+def admin_home():
+    return render_template('admin_home.html')
+
+@app.route('/login', methods = ['GET','POST'])
+
+def login():
+    error = None
+    admin_authorised = False
+    student_authorised = False
+    if request.method == 'POST':
+        if (request.form['username'] != 'admin' and request.form['username'] != 'student') or request.form['password'] != 'admin' :
+            error = "Invalid credentials, Please try again!."
+        elif request.form['username'] == 'admin':
+            admin_authorised = True
+            return redirect(url_for('admin_home'))
+        elif request.form['username'] == 'student':
+            student_authorised = True
+            return redirect(url_for('home'))
+
+    return render_template('login.html', error = error)
+
+if __name__ == '__main__':
+   # path = "data.xlsx"
+   # cur = xlrd.open_workbook(path)
+   
+   # first_sheet = cur.sheet_by_index(0)
+   # print(first_sheet.row_values(0))
+
+    app.run(debug = True)
