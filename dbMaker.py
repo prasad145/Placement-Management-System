a1eb55a6e1cb322acfc6233b6eaf6b77d96a79ea
NikithKS <nikithks007@gmail.com>
2020-10-15 21:27:20 +0530

    Created actual database

---
+++ dbMaker.py
@@ -0,0 +1,37 @@
+# dbMaker.py
+
+import xlrd
+import sqlite3
+
+loc = "students.xlsx"
+db = sqlite3.connect('data.db')
+cur = db.cursor()
+
+xl = xlrd.open_workbook(loc)
+s = xl.sheet_by_index(0)
+
+num = s.nrows
+create = " CREATE TABLE IF NOT EXISTS studentDB(USN text PRIMARY KEY, full_name text, first_name TEXT, CGPA FLOAT, backs INT);"
+insert = "INSERT INTO studentDB(USN, full_name, first_name, CGPA, backs) values "
+
+cur.execute(create)
+db.commit()
+
+for i in range(num):
+    data = s.row_values(i)
+    usn = data[0]
+    name = data[1]
+    fname = data[2].lower()
+    cgpa = data[27]
+    backs = data[31]
+
+    val = "('"+ usn + "' ,'" + name +"' ,'" + fname +"' ," + str(cgpa) +", " + str(backs) + ");"
+    q = insert + val
+    # print(q)
+    try:
+        cur.execute(q)
+    except:
+        print(val)
+        
+    # print(data)
+db.commit()
