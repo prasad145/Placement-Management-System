b8c1d1425d507dfbef5a0bc18c1fd2630b808b83
Prasad acharya <50115378+prasad145@users.noreply.github.com>
2020-10-15 19:44:28 +0530

    encryption part removed

--- main.py
+++ main.py
@@ -19,46 +19,37 @@ def home():
     return (render_template('login.html'))
 
 @app.route('/admin_home')
-def adminHome():
+def admin_home():
     return render_template('admin_home.html')
 
 @app.route('/login', methods = ['GET','POST'])
-
-def studentLogin():
-    return render_template('login.html')
-
 def logi():
     if request.method == 'POST':
-        id =  str(input())                     #request.form.get("id")
-        password = str(input())                 #request.form.get("password")
-        secure_pass = sha256_crypt.encrypt(str(password))
-        db.execute("""ALTER TABLE students 
-                     ADD secretkey varchar(40)""")
-        db.execute("""INSERT INTO students
-                      (secretkey) values(:secretkey)""",{"secretkey" : secure_pass})
+        id = request.form.get("USN")
+        password = request.form.get("PW")
+        #secure_pass = sha256_crypt.encrypt(str(password))
+        #db.execute("""ALTER TABLE students 
+        #            ADD secretkey varchar(40) WHERE usn = :usn""",{"usn" : id})
+        #db.execute("""INSERT INTO students
+          #            (secretkey) values(:secretkey)""",{"secretkey" : secure_pass})
         usernamedata = db.execute("SELECT usn FROM students where usn = :usn",{"usn" : id}).fetchone()   
-        passworddata = db.execute("SELECT secretkey from students where usn = :usn",{"usn" : id}).fetchone()
+        passworddata = db.execute("SELECT usn from students where first_name = :first_name",{"first_name" : password}).fetchone()
 
         if usernamedata is None:
             #put flash messege here
             return render_template("login.html")
-        else: 
-            for password_data in passworddata:
-                if sha256_crypt.verify(password, password_data):
+        else:
+            if passworddata is not None: 
+            #for password_data in passworddata:
+             #   if sha256_crypt.verify(password, password_data):
                     #put flash messege here ("login successfull")
-                    return redirect(url_for('register'))
-                else:
-                    #put flash messege ("incorrect password")
-                    return render_template("login.html")
+                return redirect(url_for('register'))
+            else:
+                #put flash messege ("incorrect password")
+                return render_template("login.html")
 
-        db.commit()          
-    
+        db.commit()         
 
 if __name__ == '__main__':
-   # path = "data.xlsx"
-   # cur = xlrd.open_workbook(path)
-   
-   # first_sheet = cur.sheet_by_index(0)
-   # print(first_sheet.row_values(0))
     app.secret_key = "#weareallnerds69"
     app.run(debug = True)
