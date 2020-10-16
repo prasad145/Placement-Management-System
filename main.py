f9171cf4485908701e9cff374e0f6513b775242e
Prasad acharya <50115378+prasad145@users.noreply.github.com>
2020-10-16 10:32:05 +0530

    sqlite3 hing haakr work aatta?

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from student_class import Student
+import sqlite3
 from flask import Flask , render_template , redirect , url_for , request , session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from passlib.hash import sha256_crypt
 
-engine = create_engine("sqlite:///std_data.db", echo = True)
+#engine = create_engine("sqlite:///data.db", echo = True)
 
-db = scoped_session(sessionmaker(bind = engine))
+#db = scoped_session(sessionmaker(bind = engine))
+
+db = sqlite3.connect('data.db')
+cur = db.cursor()
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def m():
@@ -27,13 +32,9 @@ def logi():
     if request.method == 'POST':
         id = request.form.get("USN")
         password = request.form.get("PW")
-        #secure_pass = sha256_crypt.encrypt(str(password))
-        #db.execute("""ALTER TABLE students 
-        #            ADD secretkey varchar(40) WHERE usn = :usn""",{"usn" : id})
-        #db.execute("""INSERT INTO students
-          #            (secretkey) values(:secretkey)""",{"secretkey" : secure_pass})
-        usernamedata = db.execute("SELECT usn FROM student where usn = :usn",{"usn" : id}).fetchone()   
-        passworddata = db.execute("SELECT usn from student where first_name = :first_name",{"first_name" : password}).fetchone()
+        
+        usernamedata = cur.execute("SELECT usn FROM studentDB where usn = :usn",{"usn" : id}).fetchone()   
+        passworddata = cur.execute("SELECT usn from studentDB where fname = :fname",{"fname" : password}).fetchone()
 
         if usernamedata is None:
             #put flash messege here
