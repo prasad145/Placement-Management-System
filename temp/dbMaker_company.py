0872e401182d530c064ccb16feea50f929f1091b
Prasad acharya <50115378+prasad145@users.noreply.github.com>
2020-10-29 23:17:34 +0530

    Delete dbMaker_company.py

--- temp/dbMaker_company.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-db1 = sqlite3.connect('companyDB.db')
-cur1 = db1.cursor()
-
-create1 = " CREATE TABLE IF NOT EXISTS companies(comp_id int PRIMARY KEY AUTOINCREMENT, company_name text, CTC int, offered_Role TEXT, CGPA FLOAT, backs INT);"
-cur1.execute(create1)
-print("successfully created")
-db1.commit()
